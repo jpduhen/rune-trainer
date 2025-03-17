4c3f4c3f302b5d688f1fbf92eaf598cc3a94f30a
jpduhen <45515609+jpduhen@users.noreply.github.com>
2025-03-17 19:30:05 +0100

    Add files via upload

--- runes.js
+++ runes.js
@@ -3,144 +3,144 @@ const RUNES = [
         id: 1,
         name: "Fehu",
         meaning: "Rijkdom, Vee, Voorspoed, Overvloed, Vruchtbaarheid, Creativiteit",
-        imageUrl: "assets/images/01-fehu.png"
+        imageUrl: "assets/backgrounds/01-fehu.webp"
     },
     {
         id: 2,
         name: "Uruz",
         meaning: "Oeros, Kracht, Vitaliteit, Wildheid, Moed, Uithoudingsvermogen",
-        imageUrl: "assets/images/02-uruz.png"
+        imageUrl: "assets/backgrounds/02-uruz.webp"
     },
     {
         id: 3,
         name: "Thurisaz",
         meaning: "Doorn, Reactie, Verdediging, Conflict, Catharsis, Regeneratie",
-        imageUrl: "assets/images/03-thurisaz.png"
+        imageUrl: "assets/backgrounds/03-thurisaz.webp"
     },
     {
         id: 4,
         name: "Ansuz",
         meaning: "Riviermonding, Mond, Communicatie, Begrip, Inspiratie, Wijsheid",
-        imageUrl: "assets/images/04-ansuz.png"
+        imageUrl: "assets/backgrounds/04-ansuz.webp"
     },
     {
         id: 5,
         name: "Raidho",
         meaning: "Wagen, Reizen, Ritme, Spontaniteit, Evolutie, Beslissingen",
-        imageUrl: "assets/images/05-raidho.png"
+        imageUrl: "assets/backgrounds/05-raidho.webp"
     },
     {
         id: 6,
         name: "Kenaz",
         meaning: "Toorts, Visie, Creativiteit, Inspiratie, Verbetering, Vitaliteit",
-        imageUrl: "assets/images/06-kenaz.png"
+        imageUrl: "assets/backgrounds/06-kennaz.webp"
     },
     {
         id: 7,
         name: "Gebo",
         meaning: "Gift, Balans, Uitwisseling, Partnerschap, Vrijgevigheid, Relaties",
-        imageUrl: "assets/images/07-gebo.png"
+        imageUrl: "assets/backgrounds/07-gebo.webp"
     },
     {
         id: 8,
         name: "Wunjo",
         meaning: "Vreugde, Plezier, Comfort, Harmonie, Voorspoed, Beloning, Succes",
-        imageUrl: "assets/images/08-wunjo.png"
+        imageUrl: "assets/backgrounds/08-wunjo.webp"
     },
     {
         id: 9,
         name: "Hagalaz",
         meaning: "Hagel, Natuurkracht, Beproeving, Obstakels Overwinnen, Chaos, Vernieuwing",
-        imageUrl: "assets/images/09-hagalaz.png"
+        imageUrl: "assets/backgrounds/09-hagalaz.webp"
     },
     {
         id: 10,
         name: "Nauthiz",
         meaning: "Nood, Beperking, Conflict, Wilskracht, Uithoudingsvermogen, Zelfredzaamheid",
-        imageUrl: "assets/images/10-nauthiz.png"
+        imageUrl: "assets/backgrounds/10-nauthiz.webp"
     },
     {
         id: 11,
         name: "Isa",
         meaning: "IJs, Helderheid, Stilstand, Uitdagingen, Introspectie, Observeren & Wachten",
-        imageUrl: "assets/images/11-isa.png"
+        imageUrl: "assets/backgrounds/11-isa.webp"
     },
     {
         id: 12,
         name: "Jera",
         meaning: "Jaar, Cycli, Voltooiing, Veranderingen, Oogst, Beloning Ontvangen",
-        imageUrl: "assets/images/12-jera.png"
+        imageUrl: "assets/backgrounds/12-jera.webp"
     },
     {
         id: 13,
         name: "Eihwaz",
         meaning: "Taxusboom, Balans, Verlichting, Dood, De Wereldboom, Kracht",
-        imageUrl: "assets/images/13-eihwaz.png"
+        imageUrl: "assets/backgrounds/13-eihwaz.webp"
     },
     {
         id: 14,
         name: "Perthro",
         meaning: "Dobbelsteenbeker, Lot, Kans, Mysterie, Bestemming, Geheimen",
-        imageUrl: "assets/images/14-perthro.png"
+        imageUrl: "assets/backgrounds/14-perthro.webp"
     },
     {
         id: 15,
         name: "Algiz",
         meaning: "Eland, Bescherming, Verdediging, Instinct, Groepsinspanning, Bescherming",
-        imageUrl: "assets/images/15-algiz.png"
+        imageUrl: "assets/backgrounds/15-algiz.webp"
     },
     {
         id: 16,
         name: "Sowilo",
         meaning: "Zon, Gezondheid, Eer, Hulpbronnen, Overwinning, Heelheid, Zuivering",
-        imageUrl: "assets/images/16-sowilo.png"
+        imageUrl: "assets/backgrounds/16-sowilo.webp"
     },
     {
         id: 17,
         name: "Tiwaz",
         meaning: "God Tyr, Mannelijkheid, Gerechtigheid, Leiderschap, Logica, Strijd",
-        imageUrl: "assets/images/17-tiwaz.png"
+        imageUrl: "assets/backgrounds/17-tiwaz.webp"
     },
     {
         id: 18,
         name: "Berkana",
         meaning: "Berkenboom, Vrouwelijkheid, Vruchtbaarheid, Genezing, Regeneratie, Geboorte",
-        imageUrl: "assets/images/18-berkana.png"
+        imageUrl: "assets/backgrounds/18-berkana.webp"
     },
     {
         id: 19,
         name: "Ehwaz",
         meaning: "Paard, Transport, Beweging, Vooruitgang, Vertrouwen, Verandering",
-        imageUrl: "assets/images/19-ehwaz.png"
+        imageUrl: "assets/backgrounds/19-ehwaz.webp"
     },
     {
         id: 20,
         name: "Mannaz",
         meaning: "Mensheid, Individualiteit, Vriendschap, Samenleving, Samenwerking, Hulp",
-        imageUrl: "assets/images/20-mannaz.png"
+        imageUrl: "assets/backgrounds/20-mannaz.webp"
     },
     {
         id: 21,
         name: "Laguz",
         meaning: "Water, Intuïtie, Emoties, Stroming, Vernieuwing, Dromen, Hoop & Angst",
-        imageUrl: "assets/images/21-laguz.png"
+        imageUrl: "assets/backgrounds/21-laguz.webp"
     },
     {
         id: 22,
         name: "Inguz",
         meaning: "Vruchtbaarheid, Kracht, Potentieel, Innerlijke Kracht, Vitaliteit, Groei",
-        imageUrl: "assets/images/22-inguz.png"
+        imageUrl: "assets/backgrounds/22-inguz.webp"
     },
     {
         id: 23,
         name: "Othala",
         meaning: "Erfenis, Voorouders, Bezittingen, Erfgoed, Ervaring, Waarde, Thuis",
-        imageUrl: "assets/images/23-othala.png"
+        imageUrl: "assets/backgrounds/23-othala.webp"
     },
     {
         id: 24,
         name: "Dagaz",
         meaning: "Dageraad, Ontwaken, Zekerheid, Verlichting, Voltooiing, Hoop, Transformatie",
-        imageUrl: "assets/images/24-dagaz.png"
+        imageUrl: "assets/backgrounds/24-dagaz.webp"
     }
 ]; 
